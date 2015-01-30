ddde0adc6a30c7861c0678c0ba9bd3d95e09bfd0
Kameron Ross Johnson <KameronJohnson@gmail.com>
2015-01-30 10:46:36 -0800

    callbacks and validations complete with passing specs

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -1,5 +1,18 @@
 require("spec_helper")
 
 describe(Band) do
+  
   it { should have_and_belong_to_many(:venues) }
+
+  it('should only accept unique band names') do
+    band1 = Band.new({:name => "thrice"})
+    band1.save()
+    band2 = Band.new({:name => "thrice"})
+    expect(band2.save()).to(eq(false))
+  end
+
+  it('capitalizes the band name') do
+    band = Band.create({:name => "the cranberries"})
+    expect(band.name()).to(eq("The Cranberries"))
+  end
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -4,4 +4,15 @@ describe(Venue) do
 
   it {should have_and_belong_to_many(:bands) }
 
+  it('should only accept unique venue names') do
+    venue1 = Venue.new({:name => "the paris theatre"})
+    venue1.save()
+    venue2 = Venue.new({:name => "the paris theatre"})
+    expect(venue2.save()).to(eq(false))
+  end
+
+  it('capitalizes the venue name') do
+    venue = Venue.create({:name => "the roseland"})
+    expect(venue.name()).to(eq("The Roseland"))
+  end
 end
